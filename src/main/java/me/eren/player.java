0205db2340687b9f6f5c3d79d2e9ad330c50ba03
Hidayet Eren Uludağ <76475118+mightymountain@users.noreply.github.com>
2023-04-24 19:57:46 +0300

    Revert "hi"
    This reverts commit a6c273305272bec10acca3fa37a68840bac0277b.

--- src/main/java/me/eren/player.java
+++
@@ -1,8 +0,0 @@
-package me.eren;
-
-import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
-public class player extends AudioEventAdapter{
-    myListener songs = new myListener();
-
-}
-
